8467ee5fbcdfc2eaa32e5045dd7696526d3a6b7c
LTC0213 <ltc0213@zju.edu.cn>
2023-08-22 19:06:54 +0800

    swagger

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	gs "github.com/swaggo/gin-swagger"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"strings"
+	_ "test_gin_swagger/docs"
+)
+
+// making an instance of the type DB from the gorm package
+var db *gorm.DB = nil
+var err error
+
+// @title gin+gorm crud 测试swagger（必填）
+// @version 1.0 （必填）
+// @description gin+gorm crud 测试swagger
+// @license.name Apache 2.0
+// @contact.name go-swagger帮助文档
+// @contact.url https://github.com/swaggo/swag/blob/master/README_zh-CN.md
+// @host localhost:8080
+// @BasePath /
+func main() {
+	//establishing connection with mysql database 'CRUD'
+	dsn := "root:Nhss@3120@tcp(192.168.1.213:3306)/gin_test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
+	//handle the error comes from the connection with DB
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&Post{})
+
+	server := gin.Default()
+	server.Use(Cors())
+
+	//set up the different routes
+	server.GET("/posts", Posts)
+	server.GET("/posts/:id", Show)
+	server.POST("/posts", Store)
+	server.PATCH("/posts/:id", Update)
+	server.DELETE("/posts/:id", Delete)
+
+	server.GET("/swagger/*any", gs.WrapHandler(swaggerFiles.Handler))
+
+	//start the server and listen on the port 8080
+	server.Run(":8080")
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		var headerKeys []string
+		for k, _ := range c.Request.Header {
+			headerKeys = append(headerKeys, k)
+		}
+		headerStr := strings.Join(headerKeys, ", ")
+		if headerStr != "" {
+			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		} else {
+			headerStr = "access-control-allow-origin, access-control-allow-headers"
+		}
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "*")                                       // 这是允许访问所有域
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
+			//  header的类型
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			//              允许跨域设置                                                                                                      可以返回其他子段
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
+			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
+			c.Header("Access-Control-Allow-Credentials", "false")                                                                                                                                                  //  跨域请求是否需要带cookie信息 默认设置为true
+			c.Set("content-type", "application/json")                                                                                                                                                              // 设置返回格式是json
+		}
+
+		//放行所有OPTIONS方法
+		//if method == "OPTIONS" {
+		//    c.JSON(http.StatusOK, "Options Request!")
+		//}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		// 处理请求
+		c.Next() //  处理请求
+	}
+}
